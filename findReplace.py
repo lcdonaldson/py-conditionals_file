daf06c075f7213d459a6f0924fa169e0a59d2227
Levi Donaldson <lcdbacanas@gmail.com>
2018-09-11 22:27:52 -0500

    Update findReplace.py

--- findReplace.py
+++ findReplace.py
@@ -1,4 +1,7 @@
+# fot single replacements
+
 import os
+
 textToFind = "white"
 textToReplace = "#fff"
 sourcePath = os.listdir('pathToFile')
@@ -14,3 +17,38 @@ for file in sourcePath:
   with open(fileToChange, 'w') as file:
     file.write(filedata)
   print("total %d replaced" % freq)
+  
+#  -------------------------------------------------------------
+
+# For multiple replacements
+import re
+import os 
+
+whiteText = 'white'
+offWhite = '#D7E1E7'
+greyText = '#444'
+sourcePath = os.listdir('/Users/levi/Desktop/CODE/code_practice/Regex/python/static/')
+for file in sourcePath:
+  inputfile = '/Users/levi/Desktop/CODE/code_practice/Regex/python/static/' + file
+  print('converting' + inputfile)
+  with open(inputfile, 'r') as inputfile:
+    filedata = inputfile.read()
+
+  fileToChange = '/Users/levi/Desktop/CODE/code_practice/Regex/python/static/' + file
+  if whiteText == 'white':
+    newText = '#fff'
+    filedata = filedata.replace(whiteText, newText)
+    print('changed white to #fff')
+
+  if offWhite == '#D7E1E7':
+    newText = '#fff'
+    filedata = filedata.replace(offWhite, newText)
+    print('changed offwhite to #fff')
+
+  if greyText == '#444':
+    newText = 'grey'
+    filedata = filedata.replace(greyText, newText)
+    print('changed #444 to grey')
+    
+  with open(fileToChange, 'w') as file:
+    file.write(filedata)
